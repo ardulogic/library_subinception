c6a8e8fcb8a4462e99a2a98ba8baf53814f70b24
ardulogic <denncath@gmail.com>
2019-01-17 22:28:25 +0200

    Sub Inception

---
+++ README.md
@@ -0,0 +1,1 @@
+**Composer-compatible library with versioning**
---
+++ src/subinceptionlib/SubInception.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SubInceptionLib;
+
+class SubInception {
+	public static function incept() {
+		echo "Inception started from subinception";
+	}
+}
